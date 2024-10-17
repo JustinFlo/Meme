5ea16802799a1846d0eace1acda6581fe36f3416
JustinFlo <225jflores@frhsd.com>
2024-10-17 09:00:07 -0400

    Added

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ let title = document.getElementById("title");
 let getMemeBtn = document.getElementById("get-meme-btn");
 let url = "https://meme-api.com/gimme/";
 
-let subreddits= ["me_irl", "whenthe", "DankMemesFromSite19", "HistoryMemes","ItemShop", "AbruptChaos", "Bossfight", "oddlyspecific",  "ProgrammerHumor"];
+let subreddits= ["me_irl", "whenthe", "DankMemesFromSite19", "HistoryMemes","ItemShop", "AbruptChaos", "Bossfight", "oddlyspecific",  "ProgrammerHumor", "NonPoliticalTwitter"];
 
 let getMeme = () => {
         let randomSubreddit = subreddits[Math.floor(Math.random () * subreddits.length)];
